284e9e668728cfb9bc2b270e2d1551c2341d0060
srensommer <srensommer@gmail.com>
2019-09-03 18:41:45 +0200

    Test kører lokalt

---
+++ Calculator/Calculator/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface ICalculator
+    {
+        int Add(int x, int y);
+        int Subtract(int x, int y);
+
+    }
+}
---
+++ Calculator/CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator.Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator.Calculator();
+        }
+
+        [Test]
+        public void Add_AddOneToOne_return2()
+        {
+            const int EXPECTED_RETURNVALUE = 2;
+            //Using AAA pattern
+
+            //Arrange in setup
+
+            //Act
+            double returnValue = uut.Add(1, 1);
+            //Assert
+            Assert.That(returnValue, Is.EqualTo(EXPECTED_RETURNVALUE));
+        }
+
+        public void Substract_Sub2From5_return3()
+        {
+            const int EXPECTED_RETURNVALUE = 3;
+            //Using AAA pattern
+
+            //Arrange in setup
+
+            //Act
+            double returnValue = uut.Subtract(5, 2);
+            //Assert
+            Assert.That(returnValue, Is.EqualTo(EXPECTED_RETURNVALUE));
+        }
+    }
+}
